47dde657f3067ac7fc9177da8b554cd12484b746
Eugene Watson <eugenewats@gmail.com>
2014-06-30 18:55:04 -0400

    adding in a single API Client method to fetch JSON from URL

--- EasySkys/APIClient.m
+++ EasySkys/APIClient.m
@@ -7,7 +7,49 @@
 //
 
 #import "APIClient.h"
+#import "Conditions.h"
+#import "DailyForecast.h"
+
+@interface APIClient ()
+
+@property (strong, nonatomic) NSURLSession *session;
+
+@end
 
 @implementation APIClient
+
+
+//creating the NSURLSession
+-(id)init {
+    if (self = [super init]) {
+        NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+        _session = [NSURLSession sessionWithConfiguration:config];
+    }
+    return self;
+}
+
+
+-(RACSignal *)fetchJSONFromURL:(NSURL *)url
+{
+    
+    NSLog(@"Fetching: %@", url.absoluteString);
+    
+    return [[RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        NSURLSessionDataTask *dataTask = [self.session dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+            //TODO handle retrieved data
+        }];
+        
+        [dataTask resume];
+        
+        return [RACDisposable disposableWithBlock:^{
+            [dataTask cancel];
+        }];
+        
+    }] doError:^(NSError *error) {
+        NSLog(@"@%", error);
+    
+    }];
+    
+}
 
 @end
