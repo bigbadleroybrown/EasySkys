4a1ff551619cbec2eba79cbbf7eabb610e5251e0
Eugene Watson <eugenewats@gmail.com>
2014-07-01 10:41:53 -0400

    adding location manager

--- EasySkys/WeatherManager.h
+++ EasySkys/WeatherManager.h
@@ -7,9 +7,22 @@
 //
 
 #import <Foundation/Foundation.h>
+@import CoreLocation;
+#import <ReactiveCocoa/ReactiveCocoa/ReactiveCocoa.h>
+#import "Conditions.h"
+
 
 @interface WeatherManager : NSObject
+<CLLocationManagerDelegate>
 
++(instancetype)sharedManager;
 
+@property (strong, nonatomic, readonly) CLLocation *currentLocation;
+@property (strong, nonatomic, readonly) Conditions *currentConditions;
+@property (strong, nonatomic, readonly) NSArray *hourlyForecast;
+@property (strong, nonatomic, readonly) NSArray *dailyForecast;
+
+//will refresh the location or current weather
+-(void)findCurrentLocation;
 
 @end
--- EasySkys/WeatherManager.m
+++ EasySkys/WeatherManager.m
@@ -7,7 +7,83 @@
 //
 
 #import "WeatherManager.h"
+#import "APIClient.h"
+#import <TSMessages/TSMessage.h>
+
+@interface WeatherManager ()
+
+@property (strong, nonatomic, readwrite) Conditions *updateCurrentConditions;
+@property (strong, nonatomic, readwrite) CLLocation *currentLocation;
+@property (strong, nonatomic, readwrite) NSArray *updateHourlyForecast;
+@property (strong, nonatomic, readwrite) NSArray *updateDailyForecast;
+
+@property (strong, nonatomic) CLLocationManager *manager;
+@property (nonatomic, assign) BOOL *isFirstUpdate;
+@property (strong, nonatomic) APIClient *client;
+
+@end
 
 @implementation WeatherManager
+
+
++(instancetype)sharedManager {
+    static id _sharedManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _sharedManager = [[self alloc]init];
+    });
+        
+        return _sharedManager;
+}
+
+
+-(id)init {
+    if (self = [super init]){
+        _manager = [[CLLocationManager alloc]init];
+        _manager.delegate = self;
+        _client = [[APIClient alloc] init];
+        
+        [[[[RACObserve(self, currentLocation)
+            ignore:nil]
+           
+           flattenMap:^(CLLocation *newLocation) {
+               return [RACSignal merge:@[[self updateCurrentConditions],
+                                         [self updateDailyForecast],
+                                         [self updateHourlyForecast]
+                                         ]];
+           }] deliverOn:RACScheduler.mainThreadScheduler]
+         
+         subscribeError:^(NSError *error) {
+             [TSMessage showNotificationWithTitle:@"Error" subtitle:@"There was an error retrieving the current weather, please try again" type:TSMessageNotificationTypeError];
+         }];
+    }
+    return  self;
+}
+
+-(void)findCurrentLocation
+{
+    
+    self.isFirstUpdate = YES;
+    [self.manager startUpdatingLocation];
+    
+}
+
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    
+    if (self.isFirstUpdate) {
+        self.isFirstUpdate = NO;
+        return;
+    }
+    
+    CLLocation *location = [locations lastObject];
+    
+    if (location.horizontalAccuracy > 0) {
+        self.currentLocation = location;
+        [self.manager stopUpdatingLocation];
+    }
+}
+
 
 @end
